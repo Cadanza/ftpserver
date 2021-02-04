d344c6581eb11dab63757c218ceecc7cbf6c6a9b
Lunala <aurelie.saulq@gmail.com>
2021-02-04 21:25:43 +0100

    début du projet

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-# [warn(non_snake_case)]
+
+use log::LevelFilter;
 use std::env;
+use std::assert;
 
 fn main() {
+
+    let logger = simple_logging::log_to_file("test.log", LevelFilter::Info);
+
+    log::info!("coucou");
     
     let args : Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    
+    assert!(args.len()>=3);
+
+    let froot : &String = &args[1];
+    let port : &String = &args[2];
+
+    println!("{} -- {}", froot, port);
 
 }
